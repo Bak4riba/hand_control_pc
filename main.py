b9735db4aa16a093eea9865b5df3140b26034759
Bakaus <matheus.bakaus.bruno@gmail.com>
2026-02-22 22:20:18 -0300

    add: identify left and right hands

--- main.py
+++ main.py
@@ -18,18 +18,33 @@ with mp_hands.Hands(
         if not ret:
             break
 
+        frame = cv2.flip(frame, 1)  # espelha tipo webcam
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(frame_rgb)
 
         if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
+            for idx, hand_landmarks in enumerate(results.multi_hand_landmarks):
+
+                # Desenha a mão
                 mp_draw.draw_landmarks(
                     frame,
                     hand_landmarks,
                     mp_hands.HAND_CONNECTIONS
                 )
 
-        cv2.imshow("Hand Tracking", frame)
+                # Descobre se é esquerda ou direita
+                hand_label = results.multi_handedness[idx].classification[0].label
+
+                # Pega coordenada do ponto 0 (pulso)
+                h, w, c = frame.shape
+                x = int(hand_landmarks.landmark[0].x * w)
+                y = int(hand_landmarks.landmark[0].y * h)
+
+                cv2.putText(frame, hand_label, (x, y - 20),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1, (0, 255, 0), 2)
+
+        cv2.imshow("Two Hand Tracking", frame)
 
         if cv2.waitKey(1) & 0xFF == 27:
             break
