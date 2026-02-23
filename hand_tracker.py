68d0f280a18cab4320126191a6f667fa950acbd3
Bakaus <matheus.bakaus.bruno@gmail.com>
2026-02-22 22:59:58 -0300

    --update: improve dist variable --updt: link close program with close hand signal

--- hand_tracker.py
+++ hand_tracker.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
-
+import time
+from time import sleep
 
 class HandTracker:
     def __init__(self, max_hands=2):
@@ -31,12 +32,49 @@ class HandTracker:
 
                 hands_data.append({
                     "label": label,
-                    "landmarks": landmarks
+                    "landmarks": landmarks,
+                    "mp_landmarks": hand_landmarks
                 })
 
         return hands_data
 
     def draw(self, frame, hands_data):
         for hand in hands_data:
-            # Aqui poderemos melhorar depois
-            pass
+            self.mp_draw.draw_landmarks(
+                frame,
+                hand["mp_landmarks"],
+                self.mp_hands.HAND_CONNECTIONS
+            )
+
+import math
+
+def is_fist(landmarks):
+
+    fingertip_ids = [8, 12, 16, 20]
+    finger_base_ids = [5, 9, 13, 17]
+
+    # mede tamanho aproximado da mão
+    wrist = landmarks[0]
+    middle_base = landmarks[9]
+
+    hand_size = math.sqrt(
+        (middle_base[0] - wrist[0])**2 +
+        (middle_base[1] - wrist[1])**2
+    )
+
+    closed_fingers = 0
+
+    for tip_id, base_id in zip(fingertip_ids, finger_base_ids):
+
+        tip = landmarks[tip_id]
+        base = landmarks[base_id]
+
+        dist = math.sqrt(
+            (tip[0] - base[0])**2 +
+            (tip[1] - base[1])**2
+        )
+        print(dist)
+        if dist < hand_size * 0.25:  # proporcional ao tamanho da mão
+            closed_fingers += 1
+
+    return closed_fingers >= 3
